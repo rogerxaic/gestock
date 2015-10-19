73468e0a6c5895b396925ac5e5800ad3d2ae1739
Roger Miret <roger.miret-gine@insa-lyon.fr>
2015-10-19 09:40:36 +0200

    Added baseproduct

---
+++ src/gestock/BaseProduct.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gestock;
+
+/**
+ *
+ * @author rmiretgine
+ */
+public class BaseProduct {
+    protected int code;
+    protected String name;
+    protected String description;
+}
