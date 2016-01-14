004735ad3eb639ff2a1a534d869f551194ccd594
Roger Miret <roger.miret-gine@insa-lyon.fr>
2016-01-14 21:49:39 +0100

    Get specific infos for Combobox

--- src/gestock/entity/MilkBaseProduct.java
+++ src/gestock/entity/MilkBaseProduct.java
@@ -1,9 +1,10 @@
 package gestock.entity;
 
 import java.util.HashMap;
+import java.util.LinkedList;
 
 public class MilkBaseProduct extends DairyBaseProduct {
-    protected int skimmed; //0-skimmed 1-semi-skimmed 2-
+    protected int skimmed; //0-whole 1-semi-skimmed 2-skim
 
     public MilkBaseProduct(long code, String name, String description, Quantity quantity, int alert, String traces, String brand, HashMap<String, Double> nutritionFacts, int originAnimal, int skimmed) {
         super(code, name, description, quantity, alert, traces, brand, nutritionFacts, originAnimal);
@@ -12,6 +13,16 @@ public class MilkBaseProduct extends DairyBaseProduct {
 
     public MilkBaseProduct(String[] fields, HashMap<String, Double> nutritionFacts) {
         this(Long.parseLong(fields[3]), fields[4], fields[5], new Quantity(fields[6]), Integer.parseInt(fields[7]), fields[8], fields[9], nutritionFacts, Integer.parseInt(fields[18]), Integer.parseInt(fields[19]));
+    }
+
+    public static LinkedList<String> getComboboxContent() {
+        LinkedList<String> result = new LinkedList<>();
+
+        result.add("baseproduct.infos.milk.type.whole");
+        result.add("baseproduct.infos.milk.type.semi");
+        result.add("baseproduct.infos.milk.type.skim");
+
+        return result;
     }
 
     public int getSkimmed() {
